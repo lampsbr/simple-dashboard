fc4b317feaa11945c9282148b2843e4e24ca59f4
Bruno Lamps <lamparolho@gmail.com>
2022-09-06 16:05:51 -0300

    This moves the initial content to a dedicated json file

--- pages/api/comparisons/[category].ts
+++ pages/api/comparisons/[category].ts
@@ -1,44 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import jsonList from '../../../json/items.json'
 
 /**
  * This returns the list of items per category.
  * Structured data: id, imgUrl, title, details
  */
 
-const itemsPerCategory = [
-  {
-    category: 'keyboards', items: [
-      {
-        id: 'sbrubles',
-        imgUrl: 'https://cdn.shopify.com/s/files/1/2695/9506/products/Bluetooth2.4GhzWiredUltra-ThinLowProfileGamingKeyboard_450x450.png?v=1626334705',
-        title: 'Redragon K618 Horus',
-        manufacturer: 'Redragon',
-        model: 'Horus 618',
-        size: 'Full'
-      },
-      {
-        id: 'sbrubles2',
-        imgUrl: 'https://cdn.shopify.com/s/files/1/2695/9506/products/redragonk621tkltenkeylesswirelesslowprofilekeyboard_450x450.png?v=1631775436',
-        title: 'Redragon K621 Horus',
-        manufacturer: 'Redragon', model: 'Horus 621', size: 'TKL'
-      },
-    ]
-  },
-  {
-    category: 'GDPs', items: [
-      { id: 'sbrubles3', title: 'Brazil 2020', country: 'Brazil', year: '2020', total: '5 apples' },
-      { id: 'sbrubles4', title: 'Brazil 2019', country: 'Brazil', year: '2019', total: '6 apples' },
-      { id: 'sbrubles5', title: 'China 2019', country: 'China', year: '2019', total: '16 apples' },
-    ]
-  },
-]
-
 export async function getItemsPerCategory(category: string) {
-  const selectedCategory = await itemsPerCategory.find(cat => cat.category === category)
+  const selectedCategory = await jsonList.content.find(cat => cat.category === category)
   return selectedCategory
 }
 export async function getAvailableCategories() {
-  return (await itemsPerCategory.map(cat => cat.category))
+  return (await jsonList.content.map(cat => cat.category))
 }
 
 type Data = {
