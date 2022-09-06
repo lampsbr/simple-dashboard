4ed48861493cdb5c1296eece7c95658784b82496
Bruno Lamps <lamparolho@gmail.com>
2022-09-06 16:34:03 -0300

    Item list details are now dynamic. We might limitate this to the 4 first properties, and only show the remaining in a future expanded version

--- components/comparison/ItemCard.tsx
+++ components/comparison/ItemCard.tsx
@@ -2,7 +2,7 @@ import { Card, Col, Grid, Text } from '@nextui-org/react';
 export default function ItemCard(props: any) {
   return (
     <Grid xs={4}>
-      <Card css={{ h: '$96', $$cardColor: '$colors$primary' }}>
+      <Card css={{ $$cardColor: '$colors$primary' }}>
         <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
           <Col>
             <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
@@ -18,16 +18,9 @@ export default function ItemCard(props: any) {
           alt="Card image background"
         />}
         <Card.Body css={{ marginTop: 10 }}>
-          {/*data for keyboards*/}
-          {props.details.manufacturer && <Text color="white">Maker: {props.details.manufacturer}</Text>}
-          {props.details.model && <Text color="white">Model: {props.details.model}</Text>}
-          {props.details.size && <Text color="white">Size: {props.details.size}</Text>}
-
-          {/*data for GDPs*/}
-          {props.details.country && <Text color="white">Country: {props.details.country}</Text>}
-          {props.details.year && <Text color="white">year: {props.details.year}</Text>}
-          {props.details.total && <Text color="white">total: {props.details.total}</Text>}
-
+          { Object.keys(props.details).map(key =>
+            <Text color="white" key={key}>{`${key}: ${props.details[key]}`}</Text>
+          )}
         </Card.Body>
       </Card>
     </Grid>
