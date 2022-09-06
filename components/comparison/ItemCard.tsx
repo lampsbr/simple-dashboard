44205dd30bbab330e80fe2047a82c4c494fc4108
Bruno Lamps <lamparolho@gmail.com>
2022-09-06 17:13:35 -0300

    Limits card details to 3 max. Card is now clickable, and opens a modal with remaining properties when clicked (pressed actually)

--- components/comparison/ItemCard.tsx
+++ components/comparison/ItemCard.tsx
@@ -1,8 +1,19 @@
-import { Card, Col, Grid, Text } from '@nextui-org/react';
+import React from 'react';
+import { Card, Col, Grid, Modal, Text } from '@nextui-org/react';
+
 export default function ItemCard(props: any) {
-  return (
+  const [visible, setVisible] = React.useState(false);
+
+  const openDetailsModal = (e: any) => {
+    setVisible(true)
+  }
+  const closeDetailsModal = () => {
+    setVisible(false)
+  }
+
+  return (<>
     <Grid xs={4}>
-      <Card css={{ $$cardColor: '$colors$primary' }}>
+      <Card css={{ $$cardColor: '$colors$primary' }} isPressable onPress={openDetailsModal}>
         <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
           <Col>
             <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
@@ -18,11 +29,22 @@ export default function ItemCard(props: any) {
           alt="Card image background"
         />}
         <Card.Body css={{ marginTop: 10 }}>
-          { Object.keys(props.details).map(key =>
-            <Text color="white" key={key}>{`${key}: ${props.details[key]}`}</Text>
-          )}
+          {Object.keys(props.details).map((key, index) => {
+            return index < 3 ? <Text color="white" key={key}>{`${key}: ${props.details[key]}`}</Text> : ''
+          })}
         </Card.Body>
       </Card>
     </Grid>
+    <Modal closeButton aria-labelledby="modal-title"
+      open={visible}
+      onClose={closeDetailsModal}
+    >
+      <Modal.Body>
+        {Object.keys(props.details).map((key, index) => {
+          return <Text key={key}>{`${key}: ${props.details[key]}`}</Text>
+        })}
+      </Modal.Body>
+    </Modal>
+  </>
   )
 }
