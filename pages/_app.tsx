908164d1675123b909fac55d8dcf66ae27d435c8
Bruno Lamps <lamparolho@gmail.com>
2022-09-12 20:05:22 -0300

    This creates a simple top navbar with links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react';
+import { Navbar, NextUIProvider, Text } from '@nextui-org/react';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Navbar variant="sticky">
+        <Navbar.Toggle aria-label="toggle navigation" />
+        <Navbar.Brand>
+          <Link href='/' ><a>
+            <Text b color="inherit" hideIn="xs">
+              Simple Dashboard (comparison site)
+            </Text>
+            </a></Link>
+        </Navbar.Brand>
+        <Navbar.Collapse>
+          <Navbar.CollapseItem key={'comparisons'}><Link href="/comparisons"><a>Comparisons</a></Link></Navbar.CollapseItem>
+          <Navbar.CollapseItem key={'about'}>Contact</Navbar.CollapseItem>
+        </Navbar.Collapse>
+      </Navbar>
       <Component {...pageProps} />
     </NextUIProvider>
   )
